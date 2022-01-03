4c7e72d561ed60fd878b63b9adb3b3f4511c7ada
gabriel <gabrielarturogq@ufps.edu.co>
2022-01-03 15:48:49 -0500

    Proceso:Continuacion de rol contratista

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\Proyecto;
+use App\Models\User;
 use Illuminate\Support\Facades\DB;
 
 class HomeController extends Controller
@@ -39,10 +40,25 @@ class HomeController extends Controller
             return view('dash.coordinador.index')->with(compact('dataContratista', 'dataProyecto'));
         } else if (auth()->user()->hasRole('Contratista')) {
 
-                $data =null;
 
-                return view('dash.contratista.index')->with(compact('data'));
-                
+            $data = DB::select("SELECT  DISTINCT proyectos.name AS proyecto, COUNT(files.name) AS cantidad
+            FROM proyectos 
+            INNER JOIN proyecto_users ON proyecto_users.proyecto_id = proyectos.id
+            INNER JOIN users ON users.nit = proyecto_users.user_nit
+            INNER JOIN file_users ON file_users.user_nit = users.nit
+            INNER JOIN files ON files.id = file_users.file_id
+            WHERE files.proyecto_id = proyectos.id AND users.nit =" . auth()->user()->nit . "
+            GROUP BY (proyecto)");
+
+            $dataProyecto = array();
+
+
+            for ($i = 0; $i < count($data); $i++) {
+                array_push($dataProyecto, [$data[$i]->proyecto, $data[$i]->cantidad,  $this->randColor()]);
+            }
+
+
+            return view('dash.contratista.index')->with(compact('dataProyecto'));
         } else {
 
             dd("nada");
