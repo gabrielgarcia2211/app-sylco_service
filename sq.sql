b77b61a2bf887f83bbe8face76dabc4e58ba1ae6
gabriel <gabrielarturogq@ufps.edu.co>
2022-01-11 14:41:06 -0500

    Correccion

--- sq.sql
+++ sq.sql
@@ -1,5 +1,3 @@
-<<<<<<< HEAD
-
 
 CREATE TABLE IF NOT EXISTS users (
   id bigint check (id > 0) NOT NULL DEFAULT NEXTVAL ('users_seq'),
@@ -22,11 +20,6 @@ INSERT INTO users (id, nit, name, last_name, email, email_verified_at, password,
 	
 
 
---------
-
-
-
-
 CREATE TABLE IF NOT EXISTS failed_jobs (
   id bigint check (id > 0) NOT NULL DEFAULT NEXTVAL ('failed_jobs_seq'),
   uuid varchar(255) NOT NULL,
@@ -39,8 +32,6 @@ CREATE TABLE IF NOT EXISTS failed_jobs (
   CONSTRAINT failed_jobs_uuid_unique UNIQUE (uuid)
 )  ;
 
-------
-
 
 CREATE TABLE IF NOT EXISTS proyectos (
   id bigint check (id > 0) NOT NULL DEFAULT NEXTVAL ('proyectos_seq'),
@@ -50,8 +41,6 @@ CREATE TABLE IF NOT EXISTS proyectos (
   PRIMARY KEY (id),
   CONSTRAINT proyectos_name_unique UNIQUE  (name)
 )  ;
-
------
 
 CREATE TABLE IF NOT EXISTS files (
   id bigint check (id > 0) NOT NULL DEFAULT NEXTVAL ('files_seq'),
@@ -72,10 +61,6 @@ CREATE INDEX files_proyecto_id_foreign ON files (proyecto_id);
 
 
 
--------
-
-
-
 CREATE TABLE IF NOT EXISTS file_users (
   id bigint check (id > 0) NOT NULL DEFAULT NEXTVAL ('file_users_seq'),
   user_nit int NOT NULL,
@@ -90,10 +75,6 @@ CREATE TABLE IF NOT EXISTS file_users (
 CREATE INDEX file_users_user_nit_foreign ON file_users (user_nit);
 CREATE INDEX file_users_file_id_foreign ON file_users (file_id);
 
-
-------
-
-
 CREATE TABLE IF NOT EXISTS jobs (
   id bigint check (id > 0) NOT NULL,
   queue varchar(255) NOT NULL,
@@ -106,12 +87,6 @@ CREATE TABLE IF NOT EXISTS jobs (
 )  ;
 
 CREATE INDEX jobs_queue_index ON jobs (queue);
-
-
-
-
------
-
 
 
 CREATE TABLE IF NOT EXISTS roles (
@@ -129,10 +104,7 @@ INSERT INTO roles (id, name, guard_name, created_at, updated_at) VALUES
 	(2, 'Aux', 'web', '2022-01-03 19:35:22', '2022-01-03 19:35:22'),
 	(3, 'Contratista', 'web', '2022-01-03 19:35:22', '2022-01-03 19:35:22');
 
------
 
-
-=======
 -- --------------------------------------------------------
 -- Host:                         127.0.0.1
 -- Versión del servidor:         5.7.33 - MySQL Community Server (GPL)
@@ -309,10 +281,7 @@ CREATE TABLE IF NOT EXISTS `permissions` (
   UNIQUE KEY `permissions_name_guard_name_unique` (`name`,`guard_name`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
--- Volcando datos para la tabla contratista.permissions: ~0 rows (aproximadamente)
-/*!40000 ALTER TABLE `permissions` DISABLE KEYS */;
-/*!40000 ALTER TABLE `permissions` ENABLE KEYS */;
->>>>>>> 16660579462a0110a1ed9cad0690513b51332021
+
 
 -- Volcando estructura para tabla contratista.proyectos
 CREATE TABLE IF NOT EXISTS `proyectos` (
@@ -323,10 +292,6 @@ CREATE TABLE IF NOT EXISTS `proyectos` (
   PRIMARY KEY (`id`),
   UNIQUE KEY `proyectos_name_unique` (`name`)
 ) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
-
-<<<<<<< HEAD
-
------
 
 
 CREATE TABLE IF NOT EXISTS model_has_permissions (
@@ -341,7 +306,6 @@ CREATE TABLE IF NOT EXISTS model_has_permissions (
 CREATE INDEX model_has_permissions_model_id_model_type_index ON model_has_permissions (model_id,model_type);
 
 
--------
 CREATE TABLE IF NOT EXISTS model_has_roles (
   role_id bigint check (role_id > 0) NOT NULL,
   model_type varchar(255) NOT NULL,
@@ -422,93 +386,3 @@ INSERT INTO migrations (id, migration, batch) VALUES
 	(8, '2021_12_15_200251_create_files_table', 1),
 	(9, '2021_12_15_200305_create_file_users_table', 1),
 	(10, '2022_01_04_153514_create_jobs_table', 2);
-=======
--- Volcando datos para la tabla contratista.proyectos: ~0 rows (aproximadamente)
-/*!40000 ALTER TABLE `proyectos` DISABLE KEYS */;
-INSERT INTO `proyectos` (`id`, `name`, `descripcion`, `ubicacion`) VALUES
-	(1, 'NATURA', 'nuevo proyecto', 'calle 12'),
-	(2, 'RESUMEN', 'segundo proyecto', 'calle 21');
-/*!40000 ALTER TABLE `proyectos` ENABLE KEYS */;
-
--- Volcando estructura para tabla contratista.proyecto_users
-CREATE TABLE IF NOT EXISTS `proyecto_users` (
-  `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
-  `user_nit` int(11) NOT NULL,
-  `proyecto_id` bigint(20) unsigned NOT NULL,
-  PRIMARY KEY (`id`),
-  KEY `proyecto_users_user_nit_foreign` (`user_nit`),
-  KEY `proyecto_users_proyecto_id_foreign` (`proyecto_id`),
-  CONSTRAINT `proyecto_users_proyecto_id_foreign` FOREIGN KEY (`proyecto_id`) REFERENCES `proyectos` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
-  CONSTRAINT `proyecto_users_user_nit_foreign` FOREIGN KEY (`user_nit`) REFERENCES `users` (`nit`) ON DELETE CASCADE ON UPDATE CASCADE
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
-
--- Volcando datos para la tabla contratista.proyecto_users: ~0 rows (aproximadamente)
-/*!40000 ALTER TABLE `proyecto_users` DISABLE KEYS */;
-INSERT INTO `proyecto_users` (`id`, `user_nit`, `proyecto_id`) VALUES
-	(1, 1234, 1),
-	(3, 4343, 1);
-/*!40000 ALTER TABLE `proyecto_users` ENABLE KEYS */;
-
--- Volcando estructura para tabla contratista.roles
-CREATE TABLE IF NOT EXISTS `roles` (
-  `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
-  `name` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
-  `guard_name` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
-  `created_at` timestamp NULL DEFAULT NULL,
-  `updated_at` timestamp NULL DEFAULT NULL,
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `roles_name_guard_name_unique` (`name`,`guard_name`)
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
-
--- Volcando datos para la tabla contratista.roles: ~0 rows (aproximadamente)
-/*!40000 ALTER TABLE `roles` DISABLE KEYS */;
-INSERT INTO `roles` (`id`, `name`, `guard_name`, `created_at`, `updated_at`) VALUES
-	(1, 'Coordinador', 'web', '2022-01-11 08:52:54', '2022-01-11 08:52:54'),
-	(2, 'Aux', 'web', '2022-01-11 08:53:06', '2022-01-11 08:53:06'),
-	(3, 'Contratista', 'web', '2022-01-11 08:53:19', '2022-01-11 08:53:20');
-/*!40000 ALTER TABLE `roles` ENABLE KEYS */;
-
--- Volcando estructura para tabla contratista.role_has_permissions
-CREATE TABLE IF NOT EXISTS `role_has_permissions` (
-  `permission_id` bigint(20) unsigned NOT NULL,
-  `role_id` bigint(20) unsigned NOT NULL,
-  PRIMARY KEY (`permission_id`,`role_id`),
-  KEY `role_has_permissions_role_id_foreign` (`role_id`),
-  CONSTRAINT `role_has_permissions_permission_id_foreign` FOREIGN KEY (`permission_id`) REFERENCES `permissions` (`id`) ON DELETE CASCADE,
-  CONSTRAINT `role_has_permissions_role_id_foreign` FOREIGN KEY (`role_id`) REFERENCES `roles` (`id`) ON DELETE CASCADE
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
-
--- Volcando datos para la tabla contratista.role_has_permissions: ~0 rows (aproximadamente)
-/*!40000 ALTER TABLE `role_has_permissions` DISABLE KEYS */;
-/*!40000 ALTER TABLE `role_has_permissions` ENABLE KEYS */;
-
--- Volcando estructura para tabla contratista.users
-CREATE TABLE IF NOT EXISTS `users` (
-  `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
-  `nit` int(11) NOT NULL,
-  `name` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
-  `last_name` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
-  `email` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
-  `email_verified_at` timestamp NULL DEFAULT NULL,
-  `password` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
-  `remember_token` varchar(100) COLLATE utf8_unicode_ci DEFAULT NULL,
-  `created_at` timestamp NULL DEFAULT NULL,
-  `updated_at` timestamp NULL DEFAULT NULL,
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `users_nit_unique` (`nit`),
-  UNIQUE KEY `users_email_unique` (`email`)
-) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
-
--- Volcando datos para la tabla contratista.users: ~0 rows (aproximadamente)
-/*!40000 ALTER TABLE `users` DISABLE KEYS */;
-INSERT INTO `users` (`id`, `nit`, `name`, `last_name`, `email`, `email_verified_at`, `password`, `remember_token`, `created_at`, `updated_at`) VALUES
-	(1, 1004804515, 'gabriel', 'garcia', 'gabrielarturogq@ufps.edu.co', '2022-01-11 08:54:16', '12345', NULL, '2022-01-11 08:54:21', '2022-01-11 08:54:22'),
-	(2, 1234, 'ARTURO', 'QUINTERO', 'garciaquinteroga@gmail.com', '2022-01-11 09:00:52', '$2y$10$EBLDXj4aQmFURZlrXChSoO7iObD5TgqpFDnVE2MEKa9WR.572eOSO', NULL, '2022-01-11 08:57:11', '2022-01-11 08:57:11'),
-	(4, 4343, 'BRIGGET', 'CACERES', 'ingcaceresbrigget@gmail.com', NULL, '$2y$10$ym.4EgUTN8FBQ8RtpOQ5Dea4u9SxT2.G5i2L8bF7Mm290grPQItJ.', NULL, '2022-01-11 08:58:34', '2022-01-11 08:58:34');
-/*!40000 ALTER TABLE `users` ENABLE KEYS */;
-
-/*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
-/*!40014 SET FOREIGN_KEY_CHECKS=IFNULL(@OLD_FOREIGN_KEY_CHECKS, 1) */;
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40111 SET SQL_NOTES=IFNULL(@OLD_SQL_NOTES, 1) */;
->>>>>>> 16660579462a0110a1ed9cad0690513b51332021
